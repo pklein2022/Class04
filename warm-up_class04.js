dda42dac19adac841e658c11e5f4d465fb1e7fcd
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-08-25 18:03:04 -0500

    Add files via upload

---
+++ warm-up_class04.js
@@ -0,0 +1,19 @@
+function findWaldo(group) {
+    let newArr = [];
+    for (let i = 0; i < group.length; i++) {
+        if(group)[i] === "Waldo"
+        newArr.push("I found Waldo!")
+    }else{
+        newArr.push(group[i])
+    }
+    
+    return newArr; 
+}
+
+   
+
+    
+findWaldo(['Stacy', 'Waldo', 'John']) 
+// returns ['Stacy', 'Found Waldo!', 'John']
+findWaldo(['Waldo', 'Jimmy', 'James', 'Jamie', 'Waldo']) 
+// ['Found Waldo!', "Jimmy', 'James', 'Jamie', 'Found Waldo!']
